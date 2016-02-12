01e220ad2b504392fd52834ae411d1113c7bd27d
dchakov <dchakov@yahoo.com>
2016-02-12 22:55:16 +0200

    tests added

---
+++ Real Estates Application/Tests/RealEstates.Web.Controllers.Tests/HomeControllerTests.cs
@@ -0,0 +1,44 @@
+﻿namespace RealEstates.Web.Controllers.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Model;
+    using Moq;
+    using RealEstates.Web.Infrastructure.Mapping;
+    using Services.Contracts;
+    using TestStack.FluentMVCTesting;
+
+    [TestClass]
+    public class HomeControllerTests
+    {
+        [TestMethod]
+        public void ByIdShouldWorkCorrectly()
+        {
+            var autoMapperConfig = new AutoMapperConfig();
+            autoMapperConfig.Execute(typeof(HomeController).Assembly);
+            const string RealEstateTitle = "SomeTitle";
+            var realEstateServiceMock = new Mock<IRealEstatesService>();
+            realEstateServiceMock.Setup(x => x.GetByEncodedId(It.IsAny<string>()))
+                .Returns(new RealEstate
+                {
+                    Title = RealEstateTitle,
+                    Description = "asghftrjryjry",
+                    Address = "asrhsthsry",
+                    Contact = "asrhsthsry",
+                    ConstructionYear = 2000,
+                    CreatedOn = DateTime.Now,
+                    Bedrooms = 2,
+                    SquareMeter = 232
+                });
+
+            // var controller = new HomeController();
+            // controller.WithCallTo(x => x.ById("adfahgsth"))
+            //    .ShouldRenderView("ById")
+            //    .WithModel<RealEstate>(
+            //        viewModel =>
+            //        {
+            //            Assert.AreEqual(RealEstateTitle, viewModel.Title);
+            //        }).AndNoModelErrors();
+        }
+    }
+}
